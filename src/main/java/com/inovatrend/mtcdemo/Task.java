e9c1a1b007e56e81ddc7d055d6a23a3f6b2b245b
Igor Buzatovic <beegor@gmail.com>
2020-06-09 09:10:48 +0200

    made started/stopped/finished variables of Task class volatile since they are accessed by multiple threads

--- src/main/java/com/inovatrend/mtcdemo/Task.java
+++ src/main/java/com/inovatrend/mtcdemo/Task.java
@@ -3,7 +3,8 @@ package com.inovatrend.mtcdemo;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import java.util.*;
+
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.atomic.AtomicLong;
@@ -13,22 +14,26 @@ import java.util.concurrent.locks.ReentrantLock;
 public class Task implements Runnable {
 
     private final List<ConsumerRecord<String, String>> records;
-    private boolean stopped = false;
-    private boolean started = false;
+
+    private volatile boolean stopped = false;
+
+    private volatile boolean started = false;
+
+    private volatile boolean finished = false;
+
     private final CompletableFuture<Long> completion = new CompletableFuture<>();
-    private boolean finished = false;
+
     private final ReentrantLock startStopLock = new ReentrantLock();
+
     private final AtomicLong currentOffset = new AtomicLong();
 
     private Logger log = LoggerFactory.getLogger(Task.class);
-
 
     public Task(List<ConsumerRecord<String, String>> records) {
         this.records = records;
     }
 
 
-    @Override
     public void run() {
         startStopLock.lock();
         if (stopped){
@@ -40,21 +45,11 @@ public class Task implements Runnable {
         for (ConsumerRecord<String, String> record : records) {
             if (stopped)
                 break;
-            processRecord(record);
+            // process record here and make sure you catch all exceptions;
             currentOffset.set(record.offset() + 1);
         }
         finished = true;
         completion.complete(currentOffset.get());
-    }
-
-
-    private void processRecord(ConsumerRecord<String, String> record) {
-        log.debug("Processing record: {}", record);
-        try {
-            Thread.sleep(10);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
     }
 
     public long getCurrentOffset() {
